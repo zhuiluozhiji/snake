740a1691d20af95bf5a2df7ac12bd36f89e05036
zhuiluozhiji <781000194@qq.com>
2025-08-17 09:09:09 +0800

    1

--- include/UI.h
+++ include/UI.h
@@ -1,5 +1,5 @@
 # pragma once
-
+#include "board.h"
 enum GameState { START, PLAYING, GAME_OVER };
 
 
@@ -10,14 +10,9 @@ enum GameState { START, PLAYING, GAME_OVER };
 #include <SFML/System.hpp>
 #include <SFML/Graphics.hpp>
 
-#define BOARD_SIZE 20
-#define CELL_SIZE 40
-#define SPEED_TIME 5
-#define FOOD_SPAWN_TIME 200
-#define BONUS_SPAWN_COUNT 5
-#define BONUS_LIVE_TIME 800
-#define BONUS_GRADE 4
-#define BONUS_MAX_SCORE 16
+
+
+
 
 
 
